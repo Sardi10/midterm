654f5b95ddcd7af4d44cc54c5b68e853b42f5adc
Sardi10 <sardikuka@gmail.com>
2025-02-06 22:38:42 -0500

    modified test calculator and init

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -1,5 +1,10 @@
-"""This is a test file for my calculator program"""
-from calculator import add
+'''This is a test file for my calculator program'''
+from calculator import add, subtract
 
 def test_addition():
+    '''Addition Test'''
     assert add(2,2) == 4
+
+def test_subtraction():
+    '''Subtraction Test'''
+    assert subtract(2,2) == 0
