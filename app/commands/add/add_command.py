3ab372a63e41d9641d757d50c20529eaa86a0c90
Sardi10 <sardikuka@gmail.com>
2025-02-24 02:06:50 -0500

    Added addition folder

---
+++ app/commands/add/add_command.py
@@ -0,0 +1,16 @@
+# app/commands/add/add_command.py
+from app.commands import Command
+from calculator.calculator import Calculator
+
+class AddCommand(Command):
+    def execute(self, args):
+        if len(args) != 2:
+            print("Usage: add <a> <b>")
+            return
+        try:
+            a = float(args[0])
+            b = float(args[1])
+            result = Calculator.add(a, b)
+            print(f"The result of {int(a)} add {int(b)} is equal to {result}")
+        except ValueError:
+            print(f"Invalid number input: {args[0]} or {args[1]} is not a valid number.")
