cc58e791453d7d6e97d7ece8f61fd9a1307ffcb7
Sardi10 <sardikuka@gmail.com>
2025-03-14 18:27:22 -0400

    Incorporated Factory PAttern into operations commands

--- app/commands/multiply/multiply_command.py
+++ app/commands/multiply/multiply_command.py
@@ -1,6 +1,6 @@
 import logging
 from app.commands import Command
-from calculator.calculation import Calculation
+from calculator.calculation_factory import CalculationFactory
 from calculator.history_manager import HistoryManager
 
 class MultiplyCommand(Command):
@@ -12,12 +12,15 @@ class MultiplyCommand(Command):
         try:
             a = float(args[0])
             b = float(args[1])
-            calc = Calculation(a, b, '*')
-            calc.perform()
+            calc = CalculationFactory.create_calculation(a, b, '*')
+            #calc = Calculation(a, b, '*')
+            result = calc.perform()
+
+            # NOW we add the Calculation to the history
             HistoryManager().add_calculation(calc)
-            #result = Calculator.multiply(a, b)
-            logging.info("MultiplyCommand executed with a=%s, b=%s, result=%s", a, b, calc.result)
-            print(f"The result of {int(a)} multiply {int(b)} is equal to {calc.result}")
+            # result = Calculator.multiply(a, b)
+            logging.info("MultiplyCommand executed with a=%s, b=%s, result=%s", a, b, result)
+            print(f"The result of {int(a)} multiply {int(b)} is equal to {result}")
         except ValueError:
             logging.exception("Invalid input for MultiplyCommand: %s", args)
             print(f"Invalid number input: {args[0]} or {args[1]} is not a valid number.")
