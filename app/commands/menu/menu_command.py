a9d1a6000060b21070d215787e33e885f77661a8
Sardi10 <sardikuka@gmail.com>
2025-03-14 21:49:44 -0400

    Small change to the menu options

--- app/commands/menu/menu_command.py
+++ app/commands/menu/menu_command.py
@@ -6,4 +6,5 @@ class MenuCommand(Command):
     def execute(self, args):
         logging.info("Displaying menu options.")
         print("Available commands: add, subtract, multiply, divide, menu, exit\n")
+        print("History Commands: history_show, histor_clear, history_load, history_save\n")
         print("Commands should be in this format: add 5 3\n")
